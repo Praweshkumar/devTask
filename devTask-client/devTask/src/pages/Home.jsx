15c42f039f18c0c6cd32847b336edbc300aff886
Prawesh kumar <143396429+Praweshkumar@users.noreply.github.com>
2025-09-01 13:06:44 +0530

    Add files via upload

---
+++ devTask-client/devTask/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="min-h-screen bg-[#ff9914] p-6 md:p-12">
+                <section className="text-center py-12  rounded-2xl  mb-8">
+                    <h1 className="text-4xl md:text-5xl font-extrabold mb-2">Welcome to <span className='text-white'>DevTasker</span></h1>
+                    <p className="text-gray-700 text-lg md:text-xl max-w-2xl mx-auto">
+                        Manage your tasks efficiently, collaborate with your team, and track progress.
+                    </p>
+                </section>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
